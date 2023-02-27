27c39b3b0cc202eb42954eef85c071cd06143f07
Eric Eschenbach <eeschenbach@chroniccareiq.com>
2023-02-26 20:43:41 -0500

    Added demo to show using webpack to inject HTML directly in JS bundle

---
+++ sampleH/componentB.js
@@ -0,0 +1,35 @@
+import template from './componentB.html';
+console.log('loaded componentB template')
+
+export class ComponentB extends ko.Component
+{
+    constructor({x, y})
+    {
+        super();
+        Object.assign(this, {x, y});
+
+        console.log('Parsed component B');
+    }
+
+    static get elementName()
+    {
+        return 'component-b';
+    }
+
+    static get template () { return template; }
+    static set template (x) { template = x; }
+
+    static get sync()
+    {
+        return true;
+    }
+
+    koDescendantsComplete()
+    {
+        console.log('koDescendantsComplete: ComponentB');
+    }
+}
+
+console.log('Registered component B');
+ComponentB.register('component-b');
+
---
+++ sampleH/index.js
@@ -0,0 +1,23 @@
+﻿console.log('Parsed sampleF');
+
+import { ComponentA } from './componentA.js';
+import { ComponentB } from './componentB.js';
+
+console.log('Done with component.register ' + ComponentA.elementName + ComponentB.elementName);
+
+let node = document.querySelector("#page-root");
+let viewModel = {
+    loaded: ko.observable(false)
+};
+
+ko
+    .applyBindings(viewModel, node)
+    .then(() =>
+    {
+        node.style.display = "block";
+        console.log('applyBindings promise');
+        viewModel.loaded(true);
+    });
+
+console.log('Done with applyBindings');
+
